596518e11878bc964fba2f4c5f05797fdd12c850
sarah <sxrahhp@gmail.com>
2023-04-01 16:32:19 +0200

    Iniciar repositorio

---
+++ src/main/java/com/proyecto/ProyectoFinalFlowersApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalFlowersApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalFlowersApiRestApplication.class, args);
+	}
+
+}
